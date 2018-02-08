33f1ac9e7ad596f934b13f217d53a41a0260bf24
Mikael Janhonen <janhon3n@gmail.com>
2018-02-08 18:30:06 +0200

    Remove draw functions

--- TuringMachine.js
+++ TuringMachine.js
@@ -11,17 +11,6 @@ Tape.prototype.shiftRight = function () {
     this.right.unshift(this.middleLetter)
     this.middleLetter = this.left.length !== 0 ? this.left.pop() : ' '
 }
-Tape.prototype.draw = function() {
-    tapePointerString = ''
-    tapeString = ''
-    for(let _ = 0; _ < this.left.length; _++) tapePointerString += ' '
-    tapePointerString += '|'
-    this.left.forEach(letter => tapeString += letter);
-    tapeString += this.middleLetter;
-    this.right.forEach(letter => tapeString += letter);
-    console.log(tapePointerString)
-    console.log(tapeString)
-}
 
 
 function TuringMachine(tape, initialState, goalStates, transitions) {
@@ -43,10 +32,7 @@ TuringMachine.prototype.goalStateReached = function() {
     if(this.goalStates.indexOf(this.currentState) !== -1) return true
     return false
 }
-TuringMachine.prototype.draw = function () {
-    console.log("State: " + this.currentState)
-    this.tape.draw()
-}
+
 
 exports.Tape = Tape
 exports.TuringMachine = TuringMachine
